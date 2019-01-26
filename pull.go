cc20aed698cac7596d94fff1352fa9adead4a804
Josh Dolitsky <jdolitsky@gmail.com>
2019-01-26 04:33:16 -0600

    extract name and version from annotations

--- pull.go
+++ pull.go
@@ -8,6 +8,7 @@ import (
 	"github.com/containerd/containerd/remotes/docker"
 	"github.com/deislabs/oras/pkg/content"
 	"github.com/deislabs/oras/pkg/oras"
+	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
 	"github.com/sirupsen/logrus"
 )
 
@@ -36,10 +37,30 @@ func main() {
 	layers, err := oras.Pull(ctx, resolver, remoteRef, memoryStore, allowedMediaTypes...)
 	check(err)
 
+	// Make sure we have layers we need to construct a Helm Chart
+	var metaLayer, contentLayer ocispec.Descriptor
+	var metaLayerFound, contentLayerFound bool
 	for _, layer := range layers {
-		//digest := layer.Digest.Hex()
-		fmt.Println(layer)
+		if layer.MediaType == helmChartMetaMediaType {
+			metaLayer = layer
+			metaLayerFound = true
+		} else if layer.MediaType == helmChartContentMediaType {
+			contentLayer = layer
+			contentLayerFound = true
+		}
 	}
+	if !metaLayerFound || !contentLayerFound {
+		panic(fmt.Sprintf("%s does not have the necessary layers", remoteRef))
+	}
+
+	// Extract chart name and version from annotations
+	name, hasName := contentLayer.Annotations["chart.name"]
+	version, hasVersion := contentLayer.Annotations["chart.version"]
+	if !hasName || !hasVersion {
+		panic(fmt.Sprintf("%s does not chart name and version saved on annoations", remoteRef))
+	}
+
+	fmt.Println(metaLayer, name, version)
 
 	fmt.Println("Success!")
 }
